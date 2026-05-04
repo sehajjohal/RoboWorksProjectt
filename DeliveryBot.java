56ad90cd62d57d7eb187f72b8ef49a2f9c1cf22e
sehajjohal <johalsehaj@gmail.com>
2026-05-04 12:58:23 -0400

    Add files via upload

--- DeliveryBot.java
+++ DeliveryBot.java
@@ -3,21 +3,10 @@ class DeliveryBot {
     int capacity;
     String status;
 
-    // Main constructor (WITH validation)
     public DeliveryBot(String name, int capacity, String status) {
         this.name = name;
         this.capacity = capacity;
-
-        if (status.equals("Available") || status.equals("Delivering")) {
-            this.status = status;
-        } else {
-            this.status = "Unknown";
-        }
-    }
-
-    // Overloaded constructor
-    public DeliveryBot(String name) {
-        this(name, 20, "Available"); // default values
+        this.status = status;
     }
 
     public void displayInfo() {
--- Drone.java
+++ Drone.java
@@ -3,38 +3,16 @@ class Drone {
     double range;
     int batteryLife;
 
-    // Constructor with validation
     public Drone(String name, double range, int batteryLife) {
         this.name = name;
         this.range = range;
-
-        if (batteryLife < 0) {
-            this.batteryLife = 0;
-        } else {
-            this.batteryLife = batteryLife;
-        }
+        this.batteryLife = batteryLife;
     }
 
-    // Overloaded constructor (quick build)
-    public Drone(String name) {
-        this(name, 10.0, 100);
-    }
-
-    // Recharge method (FIXED)
-    public void recharge(int amount) {
-        if (amount > 0) {
-            batteryLife += amount;
-            if (batteryLife > 100) {
-                batteryLife = 100;
-            }
-        }
-    }
-
-    // Display method
     public void displayInfo() {
         System.out.println("Drone Name: " + name);
         System.out.println("Range: " + range + " km");
-        System.out.println("Battery Life: " + batteryLife + "%");
+        System.out.println("Battery Life: " + batteryLife + " mins");
         System.out.println("------------------------");
     }
 }
